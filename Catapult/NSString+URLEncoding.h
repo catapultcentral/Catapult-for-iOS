7642b01c9579c20adbc596bf774ef16584c73cd4
Aziz Light <aziz@azizlight.me>
2013-04-24 18:20:27 +0100

    Added an NSString (URLEncoding) category

---
+++ Catapult/NSString+URLEncoding.h
@@ -0,0 +1,15 @@
+//
+//  NSString+URLEncoding.h
+//  Catapult
+//
+//  Created by Aziz Light on 4/14/13.
+//  Copyright (c) 2013 Catapult Technology Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (URLEncoding)
+
+-(NSString *)urlEncodeUsingEncoding:(NSStringEncoding)encoding;
+
+@end
---
+++ Catapult/NSString+URLEncoding.m
@@ -0,0 +1,21 @@
+//
+//  NSString+URLEncoding.m
+//  Catapult
+//
+//  Created by Aziz Light on 4/14/13.
+//  Copyright (c) 2013 Catapult Technology Ltd. All rights reserved.
+//
+
+#import "NSString+URLEncoding.h"
+
+@implementation NSString (URLEncoding)
+
+-(NSString *)urlEncodeUsingEncoding:(NSStringEncoding)encoding {
+	return (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL,
+                                                               (CFStringRef)self,
+                                                               NULL,
+                                                               (CFStringRef)@"!*'\"();:@&=+$,/?%#[]% ",
+                                                               CFStringConvertNSStringEncodingToEncoding(encoding)));
+}
+
+@end
